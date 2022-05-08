8daa3247563cdab298b3ee8254746175d6aee4ec
plcpl-haque <md.haque@profecialinks.com>
2022-05-08 18:32:11 +0600

    show card successfully

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -2,35 +2,32 @@ import React, { useEffect, useState } from 'react';
 import SingleCard from '../SingleCard/SingleCard';
 import { useSelector, useDispatch } from 'react-redux'
 import { loadData } from '../../redux/launchSlice';
+import { Grid } from '@mui/material';
 
 const Cards = ({ props }) => {
     const [apiData, setAPIData] = useState([]);
-    const [singleLaunch, setSingleLaunch] = useState({});
-
-    const dispatch = useDispatch();
 
     useEffect(() => {
         fetch('https://api.spacexdata.com/v3/launches')
             .then(res => res.json())
-            .then(data => dispatch(loadData(data)))
+            .then(data => setAPIData(data))
     }, []);
 
 
 
-    // if (apiData) {
-    //     apiData.forEach(l => setSingleLaunch(l));
-    // }
+    if (apiData) {
+        apiData.map(l => console.log(l));
+    }
 
-    // console.log(singleLaunch);
+    // console.log(apiData[0]);
 
     return (
-        // {
-        //     if(apiData) {
-        //         apiData.map(l => console.log(l))
-        //     }
-        // }
+        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+            {
+                apiData.map(l => <SingleCard key={l.filgh_number} l={l} />)
+            }
+        </Grid>
 
-        <SingleCard />
     );
 };
 
--- src/components/SingleCard/SingleCard.js
+++ src/components/SingleCard/SingleCard.js
@@ -1,44 +1,37 @@
 import React, { useState } from 'react';
-import { Button, Card, CardActions, CardContent, CardMedia, Typography } from '@mui/material';
+import { Button, Card, CardActions, CardContent, CardMedia, Grid, Typography } from '@mui/material';
 import { useSelector } from 'react-redux';
 
 
-const SingleCard = () => {
-    // const [singleLaunch, setLaunch] = useState({});
-    // const propsMap = launchProps.map(l => setLaunch(l));
-
-    // console.log(launchProps);
-    // launchProps.slice(0, 10).map(l => console.log(l))
-
-    const allData = useSelector(state => state.launch[0]);
-
-    const s = allData.map(l => { return l });
-
-    console.log(s?.links);
+const SingleCard = ({ l }) => {
 
     return (
+        <Grid item sx={{ mt: 5 }} xs={4} sm={4} md={4}>
+            <Card sx={{ minWidth: 275 }}>
+                <CardMedia
+                    component="img"
+                    style={{ margin: '0 auto' }}
+                    image={l.links.mission_patch_small}
+                    alt="green iguana"
+                />
+                <CardContent>
+                    <Typography sx={{ color: 'red' }} variant="h6" component="div">
+                        model
+                    </Typography>
+                    <Typography sx={{ color: 'black' }} variant="p" component="div">
+                        details.slice(0, 150);
+                    </Typography>
+                    <Typography sx={{ my: 2 }} variant="body2" color="text.secondary">
+                        {/* <span style={{ textDecoration: 'line-through', color: 'red' }}>${oldPrice}</span> <span>${newPrice}</span> */}
+                    </Typography>
 
-        <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-                component="img"
-                alt="green iguana"
-                height="140"
-                image="/static/images/cards/contemplative-reptile.jpg"
-            />
-            <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                    Lizard
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    Lizards are a widespread group of squamate reptiles, with over 6,000
-                    species, ranging across all continents except Antarctica
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <Button size="small">Share</Button>
-                <Button size="small">Learn More</Button>
-            </CardActions>
-        </Card>
+                    {/* <Link href={`/buyNow/${_id}`} sx={{ width: '150px' }} underline="none">
+                        <Button style={{ textDecoration: 'none', backgroundColor: '#cf2626d6', color: 'white', width: '100px' }}>Buy Now</Button>
+                    </Link> */}
+
+                </CardContent>
+            </Card>
+        </Grid>
     );
 };
 
